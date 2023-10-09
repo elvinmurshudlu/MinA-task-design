52354e9ebe44a86babf5de9794b399a6cda46552
elvinmurshudlu <murshudlu.elvin@gmail.com>
2023-10-09 21:27:47 +0400

    first commit

---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mina-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent {
+
+}
