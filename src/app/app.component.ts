5e85280ced4f06c8e1cc4345b7faa2de201525d5
elvinmurshudlu <murshudlu.elvin@gmail.com>
2023-10-10 13:53:07 +0400

    Lazy loading was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {ThemeService} from "./services/theme/theme.service";
+import {HeaderComponent} from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent   {
   isDarkMode = true
 
+
+  @ViewChild(HeaderComponent) element !: ElementRef
   constructor(private themeServ:ThemeService) {
     themeServ.isDarkTheme$.subscribe(theme=>{
       this.isDarkMode = theme
     })
   }
+
+
 
 
 }
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {cards} from "../../constants/cards";
 
 @Component({
@@ -8,7 +8,22 @@ import {cards} from "../../constants/cards";
 })
 export class MainComponent {
 
-  mainCards = cards
+  @ViewChild('cardsContainer') cardContainer !: ElementRef
+  elements = 20
+
+  mainCards = cards.slice(0,this.elements)
 
   protected readonly Array = Array;
+
+  onScroll(){
+    const nativeElement = this.cardContainer.nativeElement
+
+
+    if(nativeElement.clientHeight + Math.ceil(nativeElement.scrollTop) +2 >= nativeElement.scrollHeight  ){
+      this.elements+=10
+      this.mainCards = cards.slice(0,this.elements)
+    }
+  }
+
+  protected readonly cards = cards;
 }
