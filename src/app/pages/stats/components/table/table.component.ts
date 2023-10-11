864ec1e9cfad5cb82c8b81fd4847a0aaa14d997c
elvinmurshudlu <murshudlu.elvin@gmail.com>
2023-10-12 00:03:35 +0400

    Chart

--- src/app/pages/stats/components/table/table.component.ts
+++ src/app/pages/stats/components/table/table.component.ts
@@ -27,8 +27,8 @@ export class TableComponent implements AfterViewInit{
 
   columns :ColumnDefinition[] = [
     {title:'#',field:'id',width:'10%',headerSort:false,resizable:false},
-    {title:'Place Name',field:'label',width:'25%',headerSort:false ,resizable:false},
-    {title:'Most answer',field:'answer',width:'65%',headerSort:false ,resizable:false},
+    {title:'Place Name',field:'label',width:'30%',headerSort:false ,resizable:false},
+    {title:'Most answer',field:'answer',width:'58%',headerSort:false ,resizable:false},
 
 
   ]
